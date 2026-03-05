2462942039e30687caf4d643d12b5ae52042655f
Pakin <2310511101056@live4.utcc.ac.th>
2026-03-05 23:17:49 +0700

    Bug fix#3 – revise candidate no. validation

--- validate.js
+++ validate.js
@@ -10,12 +10,14 @@ function checkSID() {
 
 function checkCandiNo() {
   let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
+
+  if (isNaN(candi) || candi < 1 || candi > 10 || candi === "") {
     return false;
   } else {
     return true;
   }
 }
+
 
 function validateForm(){
 	if(!checkSID()){
