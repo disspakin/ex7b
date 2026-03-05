bf70596c5a688dd7c7a4110229dc95b753c732a0
Pakin <2310511101056@live4.utcc.ac.th>
2026-03-05 23:03:51 +0700

    Bug fix#2 – adding student id validation (id must be composed of numbers between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,8 +1,9 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
+  
+  if(/^[0-9]{10}$/.test(sid)){
     return true;
-  } else {
+  }else{
     return false;
   }
 }
